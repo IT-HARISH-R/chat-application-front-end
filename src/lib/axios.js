d09f82cf6351061c529fc777b126d0c1ba93a15d
IT-HARISH-R <harishmass27.8@gmail.com>
2025-02-21 13:26:47 +0530

    cahnge api

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  
+
   baseURL: "https://chat-application-backend-znue.onrender.com/api",
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
   withCredentials: true,
 });
 
